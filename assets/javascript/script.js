0790d24687f37ddd879a3887e5c60e37ced6d738
Pur3-MAlice <aliceford98@live.co.uk>
2023-02-07 18:23:43 +0000

    add script.js starting blocks, cardList, window.onload, cardShuffle and startGame

---
+++ assets/javascript/script.js
@@ -0,0 +1,44 @@
+let errors = 0;
+let matches = 0;
+let points = 0;
+
+/*List of Expert cards*/
+let cardList = [
+    "dustsprite",
+    "frostslime",
+    "ghost",
+    "greenslime",
+    "metalhead",
+    "purpleslime",
+    "redslime",
+    "skeleton",
+    "squidkid",
+    "tigerslime"
+];
+
+let cardSet;
+let board = [];
+let rows = 4;
+let columns = 5;
+
+/** 
+ * Function to shuffle cards and start the game as soon as window is loaded
+ */
+window.onload = function() {
+    shuffleCards();
+    startGame();
+  }
+
+/**
+ * Function to shuffle the cardList
+ */ 
+function shuffleCards() { 
+
+}    
+
+/**
+ * Function to start game play
+ */
+function startGame() {
+
+}    
